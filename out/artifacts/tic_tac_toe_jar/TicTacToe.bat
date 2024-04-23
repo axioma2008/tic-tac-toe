f40bb143b027e376501c100aff82e72f44268c65
axioma <axioma.news@gmail.com>
2024-04-23 18:49:18 +0300

    added artifact

---
+++ out/artifacts/tic_tac_toe_jar/TicTacToe.bat
@@ -0,0 +1,3 @@
+@echo OFF
+cd C:\Users\37529\IdeaProjects\tic-tac-toe\out\artifacts\tic_tac_toe_jar
+java -jar tic-tac-toe.jar
--- src/com/tikTakToe/domain/GameSaver.java
+++ src/com/tikTakToe/domain/GameSaver.java
@@ -1,26 +1,46 @@
 package com.tikTakToe.domain;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+
 public class GameSaver {
-    public void saveResult(String gameResult){
-        try (FileWriter writer = new FileWriter("game_result.txt", true)){
+    public void saveResult(String gameResult) {
+        try (FileWriter writer = new FileWriter("game_result.txt", true)) {
             writer.write(gameResult);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void readGameResult(String filePath){
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))){
-            String line;
-            while ((line = reader.readLine()) != null){
-                System.out.println((line));
+    public void readGameResult(String filePath) {
+
+        try {
+            // Возьмите файл
+            File f = new File("game_result.txt");
+            //Создайте новый файл
+            // Убедитесь, что он не существует
+            if (!f.createNewFile()) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+                    String line;
+                    String firstLine = reader.readLine();
+                    if (firstLine == null)
+                        System.out.println("История игр пуста!");
+                    else {
+                        System.out.println(firstLine);
+                    }
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println((line));
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            else {
+                System.out.println("История игр пуста!");
+            }
+        } catch (Exception e) {
+            System.err.println(e);
         }
+
     }
 }
